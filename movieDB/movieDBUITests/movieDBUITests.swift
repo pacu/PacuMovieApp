0c94a995b86dc657132ee46a2d9ac2f577ae4030
Francisco Gindre <francisco.gindre@gmail.com>
2019-07-26 17:00:54 -0300

    UI Test App launch using mocks

--- movieDB/movieDBUITests/movieDBUITests.swift
+++ movieDB/movieDBUITests/movieDBUITests.swift
@@ -18,8 +18,9 @@ class movieDBUITests: XCTestCase {
 
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         let app = XCUIApplication()
-        app.launchEnvironment = ["useMock" : "true"]
-        
+        app.launchArguments = ["useMocks"]
+        app.launch()
+ 
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
