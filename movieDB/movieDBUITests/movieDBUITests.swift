d55cce0e74ec228b6b509ca3379c0228a6fac3a7
Francisco Gindre <francisco.gindre@gmail.com>
2019-07-29 20:52:58 -0300

    add testMainControls test

--- movieDB/movieDBUITests/movieDBUITests.swift
+++ movieDB/movieDBUITests/movieDBUITests.swift
@@ -8,7 +8,8 @@
 
 import XCTest
 
-class movieDBUITests: XCTestCase {
+class MovieDBUITests: XCTestCase {
+    lazy var app = XCUIApplication()
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -17,21 +18,42 @@ class movieDBUITests: XCTestCase {
         continueAfterFailure = false
 
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        let app = XCUIApplication()
+        
         app.launchArguments = ["useMocks"]
+        
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        
         app.launch()
  
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testMainControls() {
+        
+        let movieTab = app.buttons["Movies"]
+        let showsTab = app.buttons["Shows"]
+        let topRatedSegment = app.buttons["Top Rated"]
+        let upcomingSegment = app.buttons["Upcoming"]
+        let popularSegment = app.buttons["Popular"]
+        
+        
+        // all controls are hittable
+        XCTAssertTrue(movieTab.isHittable)
+        XCTAssertTrue(showsTab.isHittable)
+        XCTAssertTrue(topRatedSegment.isHittable)
+        XCTAssertTrue(upcomingSegment.isHittable)
+        XCTAssertTrue(popularSegment.isHittable)
+        
+        // on launch first tab should be selected and popular
+        // should be the segment selected
+        
+        XCTAssert(popularSegment.isSelected)
+        XCTAssert(movieTab.isSelected)
+        
     }
 
 }
